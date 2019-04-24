d050ba313e5c7badfe2ffc878ae61132e7938e3f
Andrew Smith <andrewsmith@alumni.stanford.edu>
2019-04-24 16:11:09 -0700

    Fix bash_completion and get rid of brew flags.
    Brew flags are no longer supported.
    Fixing bash completion based off https://github.com/mathiasbynens/dotfiles/pull/864

--- home/brew.sh
+++ home/brew.sh
@@ -22,13 +22,12 @@ brew install moreutils
 brew install findutils
 
 # Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
+brew install gnu-sed
 
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
 brew install bash
-brew tap homebrew/versions
 brew install bash-completion2
 
 # homeshick for managing dotfiles
@@ -41,18 +40,18 @@ if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
 fi;
 
 # Install `wget` with IRI support.
-brew install wget --with-iri
+brew install wget
 
 # Install more recent versions of some macOS tools.
-brew install vim --override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
+brew install vim
+brew install grep
+brew install openssh
+brew install screen
 #brew install homebrew/php/php56 --with-gmp
 
 # other tools
 brew install nvm
-brew install yarn --without-node
+brew install yarn
 brew install graphviz
 brew install tree
 brew install imagemagick
